eecaa8bfc82bf52754d14544be840dda789cede4
Hollow-graphic <120960592+Hollow-graphic@users.noreply.github.com>
2026-02-19 10:13:58 +0100

    a

--- Regions.py
+++ Regions.py
@@ -2,22 +2,18 @@ from typing import Dict, List, NamedTuple
 
 from .Names import RegionName
 
-class Celeste64RegionData(NamedTuple):
+class Astronautilus(NamedTuple):
     connecting_regions: List[str] = []
 
 
-region_data_table: Dict[str, Celeste64RegionData] = {
-    "Menu": Celeste64RegionData([RegionName.forsaken_city]),
+region_data_table: Dict[str, Astronautilus] = {
+    "Menu": Astronautilus([RegionName.forsaken_city]),
 
-    RegionName.forsaken_city: Celeste64RegionData([RegionName.intro_islands, RegionName.granny_island, RegionName.highway_island, RegionName.ne_feathers_island, RegionName.se_house_island, RegionName.badeline_tower_upper, RegionName.badeline_island]),
+    RegionName.forsaken_city: Astronautilus([RegionName.level_1, RegionName.level_2, RegionName.level_3, RegionName.level_4, RegionName.level_5]),
 
-    RegionName.intro_islands:        Celeste64RegionData([RegionName.granny_island]),
-    RegionName.granny_island:        Celeste64RegionData([RegionName.highway_island, RegionName.nw_girders_island, RegionName.badeline_tower_lower, RegionName.se_house_island]),
-    RegionName.highway_island:       Celeste64RegionData([RegionName.granny_island, RegionName.ne_feathers_island, RegionName.nw_girders_island]),
-    RegionName.nw_girders_island:    Celeste64RegionData([RegionName.highway_island]),
-    RegionName.ne_feathers_island:   Celeste64RegionData([RegionName.se_house_island, RegionName.highway_island, RegionName.badeline_tower_lower, RegionName.badeline_tower_upper]),
-    RegionName.se_house_island:      Celeste64RegionData([RegionName.ne_feathers_island, RegionName.granny_island, RegionName.badeline_tower_lower]),
-    RegionName.badeline_tower_lower: Celeste64RegionData([RegionName.se_house_island, RegionName.ne_feathers_island, RegionName.granny_island, RegionName.badeline_tower_upper]),
-    RegionName.badeline_tower_upper: Celeste64RegionData([RegionName.badeline_island, RegionName.badeline_tower_lower, RegionName.se_house_island, RegionName.ne_feathers_island, RegionName.granny_island]),
-    RegionName.badeline_island:      Celeste64RegionData([RegionName.badeline_tower_upper, RegionName.granny_island, RegionName.highway_island]),
+    RegionName.level_1:              Astronautilus([RegionName.shop_1]),
+    RegionName.level_2:              Astronautilus([RegionName.shop_2]),
+    RegionName.level_3:              Astronautilus([RegionName.shop_3]),
+    RegionName.level_4:              Astronautilus([RegionName.shop_4]),
+    RegionName.level_5:              Astronautilus([RegionName.shop_5]),
 }
